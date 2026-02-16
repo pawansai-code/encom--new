a58e478dddd624aba0d139ca98f977e9a25c21d6
pawansai-code <PAWANSAIRDJ17@GMAIL.COM>
2026-02-16 15:47:35 +0530

    connected mongo db

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json()); // to accept json data
 
 // app.get("/", (req, res) => {
-//   res.send("API Running!");
+// res.send("API Running!");
 // });
 
 app.use("/api/user", userRoutes);
